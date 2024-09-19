3726887bce77d9ff71fded8e4744f491cfe29b4a
alexutm123 <pilich.sasha@gmail.com>
2024-09-19 12:10:20 +0300

    delete use scroll

--- src/components/TestHuc.jsx
+++ src/components/TestHuc.jsx
@@ -2,12 +2,15 @@ import { useEffect } from "react";
 import { useCount } from "../hooks/use-count";
 
 const TestHuc = () => {
+     const {count, increment, decrement} = useCount(0);
     useEffect(() => {
-         window.addEventListener("scroll", () => {
-             console.log("scrolling");
-         })
+        const collback=() => {
+            console.log("scrolling");
+        }
+         window.addEventListener("scroll", collback);
+
+        return () => window.removeEventListener("scroll", collback);
     }, []);
-    const {count, increment, decrement} = useCount(0);
     return (
         <div>
             <button onClick={increment}>{count}</button>
